b264de6faa59f91589b6855f5e0cea8c3ffc3fbe
yao_guangju@163.com <ygj125159>
2022-09-20 23:12:28 +0800

    ddd文件架构

---
+++ spring-cloud-component/spring-cloud-datasource/src/main/java/com/mochen/datasource/DatasourceComponent.java
@@ -0,0 +1,9 @@
+package com.mochen.datasource;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@Component
+@ComponentScan(basePackages = {"com.mochen.datasource"})
+public class DatasourceComponent {
+}
---
+++ spring-cloud-component/spring-cloud-datasource/src/main/java/com/mochen/datasource/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.mochen.datasource.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement  //开启事务
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //分页配置
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        //乐观锁
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        return interceptor;
+    }
+}
