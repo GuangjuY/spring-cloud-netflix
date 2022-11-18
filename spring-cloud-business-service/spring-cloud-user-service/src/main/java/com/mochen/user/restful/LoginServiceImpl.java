b683ee6a3369ab9c7e4302d4536bb78e532237d0
姚广举#A04787 <guangju.yao@casstime.com>
2022-11-18 18:22:27 +0800

    日志系统优化

--- spring-cloud-business-service/spring-cloud-user-service/src/main/java/com/mochen/user/restful/LoginServiceImpl.java
+++ spring-cloud-business-service/spring-cloud-user-service/src/main/java/com/mochen/user/restful/LoginServiceImpl.java
@@ -6,14 +6,17 @@ import com.mochen.spi.user.request.LoginRequest;
 import com.mochen.spi.user.service.LoginService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class LoginServiceImpl implements LoginService {
 
     @Override
     public LoginDTO login(@RequestBody LoginRequest loginRequest) {
+        log.error("1111111111111111111");
         LoginDTO loginDTO = new LoginDTO();
         loginDTO.setUserId("1232").setName("小明dto").setToken("987876543");
         return loginDTO;
